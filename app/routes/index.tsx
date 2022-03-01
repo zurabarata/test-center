ea7d5eeeb20a0a50ee30e2cfba885833dec4ddf7
Zurab <zurab@candis.io>
2022-03-01 02:01:54 +0100

    restyle a bit

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,7 +6,7 @@ export default function Index() {
     <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
         <div className="container">
             <a className="navbar-brand" href="#page-top">Testzentrum Auetal</a>
-            <button className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded"
+            <button className="navbar-toggler text-uppercase font-weight-bold bg-primary text-black rounded"
                     type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
                     aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 Menu
@@ -17,20 +17,20 @@ export default function Index() {
                     <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded"
                                                              href="#kontakt">Kontakt</a></li>
 {/*                    <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded"
-                                                             href="#about">About</a></li>
+                                                             href="#about">About</a></li>*/}
                     <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded"
-                                                             href="#contact">Contact</a></li>*/}
+                                                             href="#contact">Contact</a></li>
                 </ul>
             </div>
         </div>
     </nav>
-    <header className="masthead bg-primary text-white text-center">
+    <header className="masthead bg-primary text-black text-center">
         <div className="container d-flex align-items-center flex-column">
             <img className="masthead-avatar mb-5" src="https://telehealthuc.com/wp-content/uploads/2020/11/covid-test-illustration.png" alt="..."/>
             </div>
-            <p className="masthead-subheading font-weight-light mb-0">Testen auch ohne Terminvereinbarung möglich!<br />
-                Anschrift:<br /> Hotel Mühlenhof, <br />Zur Obersburg 7, <br />31749 Auetal<br />
-                Tel.:  +49 157 33228300<br />
+            <p className="masthead-subheading font-weight-light mb-0">Testen auch ohne <br />Terminvereinbarung möglich!<br /><br />
+                Anschrift:<br /> Hotel Mühlenhof <br />Zur Obersburg 7 <br />31749 Auetal<br /><br />
+                Tel.:  +49 157 33228300<br /><br />
                 Unsere Öffnungszeiten: <br />Montag bis Sonntag von 9:00 bis 17:00 Uhr
             </p>
 
