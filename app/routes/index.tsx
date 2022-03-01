4245a258e4bb696e880609fdbdb05ca0c91a2bb6
Zurab <zurab@candis.io>
2022-03-01 02:09:55 +0100

    center the logo

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,10 +3,10 @@ export default function Index() {
   return (
 
     <body id="page-top">
-    <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+    <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav" >
         <div className="container">
             <a className="navbar-brand" href="#page-top">Testzentrum Auetal</a>
-            <button className="navbar-toggler text-uppercase font-weight-bold bg-primary text-black rounded"
+{/*            <button className="navbar-toggler text-uppercase font-weight-bold bg-primary text-black rounded"
                     type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
                     aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 Menu
@@ -16,12 +16,12 @@ export default function Index() {
                 <ul className="navbar-nav ms-auto">
                     <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded"
                                                              href="#kontakt">Kontakt</a></li>
-{/*                    <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded"
-                                                             href="#about">About</a></li>*/}
+                    <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded"
+                                                             href="#about">About</a></li>
                     <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded"
                                                              href="#contact">Contact</a></li>
                 </ul>
-            </div>
+            </div>*/}
         </div>
     </nav>
     <header className="masthead bg-primary text-black text-center">
